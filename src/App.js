54b86429e4f4e9de0eecbdd445e3def32a41d06d
Dewansh Rawat <dewanshrawat15@gmail.com>
2021-04-08 15:28:17 +0530

    Designing home screen, laying down boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Component } from "react";
+import Home from "./Components/Home/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  // constructor(props){
+  //   super(props);
+  // }
+
+  render(){
+    return (
+      <Router>
+        <Switch>
+          {/* <Route path="/login">
+            <Login setAuthDetails={this.setAuthDetails} />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/notes">
+            <Notes details={this.state.data} />
+          </Route>
+          <Route path="/profile">
+            <Profile details={this.state.data} />
+          </Route> */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,40 @@
+import "./Home.css";
+import logo from "../../Assets/logo.png";
+
+function Home(){
+
+    return (
+        <div className="home">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2 col-md-offset-5">
+                        <center>
+                            <img src={logo} className="img-responsive" alt="Soundflow Logo" height="250" />
+                        </center>
+                    </div>
+                </div>
+                <br /><br />
+                <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                        <h1 className="text-center soundflow-hero-title">SoundFlow</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2 soundflow-hero-text">
+                        <p className="text-center">Bored of Spotify ads? Stream unlimited songs without any intervals, free of cost!</p>
+                    </div>
+                </div>
+                <br /><br />
+                <div className="row">
+                    <div className="col-md-4 col-md-offset-4">
+                        <center>
+                            <span className="soundflow-hero-bttn text-center">Stream now!</span>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
